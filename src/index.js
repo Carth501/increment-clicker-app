9d91bad33e34254505a49307f9bad87d5ecc3f03
Ryan Norman <ryan.norman@civitech.io>
2022-12-05 13:05:14 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            byteDimes: 0,
+            autoGens: {
+                count: 0,
+                cost: this.calculateNextAutoGenCost(0),
+            }
+        }
+        setInterval(() => {this.createByteDimes(this.state.autoGens.count / 10)}, 100);
+        
+    }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    createByteDimes(i) {
+        let byteDimes = this.state.byteDimes;
+        byteDimes += i;
+        this.setState({byteDimes});
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render() {
+        return (
+            <div className="game">
+                <div className="ByteDimes: ">
+                    ByteDimes: {this.state.byteDimes.toFixed()}
+                </div>
+                {this.renderFirstOrderGenerate()}
+                <div className="container">
+                    {this.renderSecondOrderGenerators()}
+                </div>
+            </div>
+        );
+    }
+
+    renderFirstOrderGenerate() {
+        return (
+            <button 
+            className="first-order-increment"
+            onClick={() => {this.createByteDimes(1)}}
+            >
+                Generate
+            </button>
+        );
+    }
+
+    renderSecondOrderGenerators() {
+        return (<div>
+            autoGen cost: {this.state.autoGens.cost}
+            <button 
+            className="second-order-increment"
+            onClick={() => {this.puchaseAutoGen()}}
+            >
+                Purchase AutoGen
+            </button>
+        </div>);
+    }
+
+    puchaseAutoGen(){
+        const state = this.state;
+        if(state.byteDimes >= state.autoGens.cost) {
+            state.byteDimes -= state.autoGens.cost;
+            state.autoGens.count += 1;
+            console.log(state.autoGens, " @ ", state.cost);
+        }
+        state.autoGens.cost = this.calculateNextAutoGenCost(state.autoGens.count);
+        this.setState(state);
+    }
+
+    calculateNextAutoGenCost(count) {
+        return Math.round( Math.pow(((count + 1) * 4 ), 1.2));
+    }
+}
+  
+  
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
