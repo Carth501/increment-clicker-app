3f30e3b382f16a21dcdf03b858c3abd2cab4eb63
Ryan Norman <ryan.norman@civitech.io>
2022-12-05 13:21:49 -0600

    Isolate lower level component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-class Game extends React.Component {
+
+class AutoGenerators extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            byteDimes: 0,
             autoGens: {
                 count: 0,
                 cost: this.calculateNextAutoGenCost(0),
             }
         }
-        setInterval(() => {this.createByteDimes(this.state.autoGens.count / 10)}, 100);
-        
+        setInterval(() => {this.props.onInterval(this.state.autoGens.count / 10)}, 100);
+    }
+
+    render() {
+        return (<div>
+            autoGen cost: {this.state.autoGens.cost}
+            <button 
+            className="second-order-increment"
+            onClick={() => {this.puchaseAutoGen()}}
+            >
+                Purchase AutoGen
+            </button>
+        </div>);
+    }
+
+    puchaseAutoGen(){
+        const state = this.state;
+        const success = this.props.onBuy(this.state.autoGens.cost);
+        if(success) {
+            state.autoGens.count += 1;
+            console.log(state.autoGens, " @ ", state.cost);
+        }
+        state.autoGens.cost = this.calculateNextAutoGenCost(state.autoGens.count);
+        this.setState(state);
+    }
+
+    calculateNextAutoGenCost(count) {
+        return Math.round( Math.pow(((count + 1) * 4 ), 1.2));
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            byteDimes: 0,
+        }
     }
 
     createByteDimes(i) {
         let byteDimes = this.state.byteDimes;
         byteDimes += i;
         this.setState({byteDimes});
+    }
+
+    costByteDimes(i) {
+        if(i > this.state.byteDimes){
+            return false;
+        }
+        this.setState({byteDimes: this.state.byteDimes - i});
+        return true;
     }
 
     render() {
@@ -29,7 +72,9 @@ class Game extends React.Component {
                 </div>
                 {this.renderFirstOrderGenerate()}
                 <div className="container">
-                    {this.renderSecondOrderGenerators()}
+                    <AutoGenerators 
+                        onInterval={(i) => this.createByteDimes(i)}
+                        onBuy={(i) => this.costByteDimes(i)}/>
                 </div>
             </div>
         );
@@ -44,33 +89,6 @@ class Game extends React.Component {
                 Generate
             </button>
         );
-    }
-
-    renderSecondOrderGenerators() {
-        return (<div>
-            autoGen cost: {this.state.autoGens.cost}
-            <button 
-            className="second-order-increment"
-            onClick={() => {this.puchaseAutoGen()}}
-            >
-                Purchase AutoGen
-            </button>
-        </div>);
-    }
-
-    puchaseAutoGen(){
-        const state = this.state;
-        if(state.byteDimes >= state.autoGens.cost) {
-            state.byteDimes -= state.autoGens.cost;
-            state.autoGens.count += 1;
-            console.log(state.autoGens, " @ ", state.cost);
-        }
-        state.autoGens.cost = this.calculateNextAutoGenCost(state.autoGens.count);
-        this.setState(state);
-    }
-
-    calculateNextAutoGenCost(count) {
-        return Math.round( Math.pow(((count + 1) * 4 ), 1.2));
     }
 }
   
